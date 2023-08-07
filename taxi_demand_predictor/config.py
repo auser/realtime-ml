2032053ab445f065a28a080308c614f3b6a267a0
Ari <me@ari.io>
2023-08-07 13:58:56 -0700

    Updated final frontend app

--- taxi_demand_predictor/config.py
+++ taxi_demand_predictor/config.py
@@ -12,8 +12,19 @@ try:
 except:
   raise Exception('Please set HOPSWORKS_API_KEY and HOPSWORKS_PROJECT_NAME in .env file')
 
-FEATURE_GROUP_NAME = "time_series_hourly_feature_group"
+FEATURE_GROUP_NAME = 'time_series_hourly_feature_group'
 FEATURE_GROUP_VERSION = 1
-
-FEATURE_VIEW_NAME = "time_series_hourly_view"
+FEATURE_VIEW_NAME = 'time_series_hourly_feature_view'
 FEATURE_VIEW_VERSION = 1
+MODEL_NAME = "taxi_demand_predictor_next_hour"
+MODEL_VERSION = 2
+
+FEATURE_GROUP_MODEL_PREDICTIONS = 'model_predictions_feature_group'
+FEATURE_VIEW_MODEL_PREDICTIONS = 'model_predictions_feature_view'
+FEATURE_VIEW_MONITORING = 'predictions_vs_actuals_for_monitoring_feature_view'
+
+# number of historical values our model needs to generate predictions
+N_FEATURES = 24 * 28
+
+# maximum Mean Absolute Error we allow our production model to have
+MAX_MAE = 4.0
